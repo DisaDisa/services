b53eec363a906926fa9b3c569db7c399815c3924
DisaDisa <denisarhipov97@gmail.com>
2020-02-09 19:08:56 +0300

    eurekaClient not resolved dependency

--- src/main/java/com/zuul/ApiGatewayApplication.java
+++ src/main/java/com/zuul/ApiGatewayApplication.java
@@ -2,11 +2,15 @@ package com.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
+@PropertySource("classpath:/zuul.properties")
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);

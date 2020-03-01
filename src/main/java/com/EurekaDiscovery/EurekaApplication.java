df5b15652175064a04e993a5870a813771472dcb
Ivliev Maxim <muss123balakovo@gmail.com>
2020-03-01 18:54:05 +0300

    Trying to fix Eureka

--- src/main/java/com/EurekaDiscovery/EurekaApplication.java
+++ src/main/java/com/EurekaDiscovery/EurekaApplication.java
@@ -4,15 +4,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+import javax.servlet.*;
+import java.io.IOException;
+
+
 @EnableEurekaServer
+@SpringBootApplication
 @PropertySource("classpath:/eureka-server.properties")
 @PropertySource("classpath:/eureka-server.yml")
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
+    @Bean
+    Filter httpTraceFilter() {
+        return new Filter() {
+            @Override
+            public void init(FilterConfig filterConfig) throws ServletException {
+
+            }
+
+            @Override
+            public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
+
+            @Override
+            public void destroy() {
+
+            }
+        };
+    }
 
 }
+
+
